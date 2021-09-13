725f165fce6f7f8aa63f8d651a46a1d15ea87db0
Jacqui <44564577+jacquiwuc@users.noreply.github.com>
2021-09-14 09:23:50 +1200

    Update index.js

--- index.js
+++ index.js
@@ -25,5 +25,8 @@ I'm Jacqui, living and working in Auckland, New Zealand.
 ⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
 
 ⏰ Updated on ${new Date().toUTCString()}\
+
+<a href="https://www.buymeacoffee.com/frostming"><img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=frostming&button_colour=b2f0ec&font_colour=000000&font_family=Bree&outline_colour=000000&coffee_colour=FFDD00"></a>
+
 `
 console.log(readme)
