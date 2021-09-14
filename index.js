8647f14bd15f8b967db0e1a849259ce00c682f17
Jacqui <44564577+jacquiwuc@users.noreply.github.com>
2021-09-14 22:22:49 +1200

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-const thisYear = new Date().getFullYear()
-const startTimeOfThisYear = new Date(`${thisYear}-01-01T00:00:00+00:00`).getTime()
-const endTimeOfThisYear = new Date(`${thisYear}-12-31T23:59:59+00:00`).getTime()
-const progressOfThisYear = (Date.now() - startTimeOfThisYear) / (endTimeOfThisYear - startTimeOfThisYear)
-const progressBarOfThisYear = generateProgressBar()
-function generateProgressBar() {
-    const progressBarCapacity = 30
-    const passedProgressBarIndex = parseInt(progressOfThisYear * progressBarCapacity)
-    const progressBar =
-      '█'.repeat(passedProgressBarIndex) +
-      '▁'.repeat(progressBarCapacity - passedProgressBarIndex)
-    return `{ ${progressBar} }`
-}
-
 //output the current DateTime in New Zealand, replace the argument with any epoch milliseconds and it should still always give you the correct time.
 const UTCFromMS = (ms) => {
   return new Date(new Date(ms).toUTCString().replace(" GMT", ""))
@@ -88,8 +74,6 @@ I'm Jacqui, living and working in Auckland, New Zealand.
      - [Twitter](https://twitter.com/AklJacqui)
      
 ---
-
-⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
 
 ⏰ Updated on ${(toNewZealand(new Date().getTime()).toString())}\
 
