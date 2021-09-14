415bc44aa331bff99aee4a3cfcebfcd10274c669
Jacqui <44564577+jacquiwuc@users.noreply.github.com>
2021-09-14 22:17:53 +1200

    Update index.js

--- index.js
+++ index.js
@@ -89,7 +89,6 @@ I'm Jacqui, living and working in Auckland, New Zealand.
      
 ---
 ⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
-⏰ Updated on ${console.log(toNewZealand(new Date().getTime()).toString())}\
 ⏰ Updated on ${(toNewZealand(new Date().getTime()).toString())}\
 
 ---
