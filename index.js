968aa05308da1fa7c89e1997f8a50f211f00d135
Jacqui <44564577+jacquiwuc@users.noreply.github.com>
2022-03-17 13:41:34 +1300

    Update index.js

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ function generateProgressBar() {
 const readme = `\
 ### Hi there 👋
 I'm Jacqui, a Data Engineer living and working in Auckland, New Zealand.
-- ☕ I’m currently working at Bank of New Zealand. It was founded in 1861 and has been an integral part of New Zealand life ever since. Today the bank employs over 5,000 people in New Zealand; works with Personal, Business, Agri and Private Wealth clients; and has 180 branches and Partners'centres across NZ.
-- 💻 Current stack: Big Data stack, Hadoop, Impala, Airflow, Spark etc
+- ☕ I’m currently working at Bank of New Zealand. It was founded in 1861 and has been an integral part of New Zealand life ever since.
+- 💻 Current stack: Big Data stack, e.g., Hadoop, Impala, Airflow, Spark etc
 - 💻 Previous project: build a cloud data analytics infrastructure, using AWS, Snowflake, Terraform, DBT etc
 - 🌱 My blog: https://jacquiwu.com/
 - 📫 How to reach me: 
