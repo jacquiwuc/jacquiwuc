17e4741f82b8ecf6c847cd0d29c5becaba23b4d3
Jacqui <44564577+jacquiwuc@users.noreply.github.com>
2022-03-24 11:29:11 +1300

    Update index.js

--- index.js
+++ index.js
@@ -36,6 +36,5 @@ I'm Jacqui, a Data Engineer living and working in Auckland, New Zealand.
 ⏰ Updated on ${new Date().toUTCString()}
 ![Progress Bar CI](https://github.com/jacquiwuc/jacquiwuc/workflows/Progress%20Bar%20CI/badge.svg)
 
-<a href="https://www.buymeacoffee.com/jacquiwuc"><img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=jacquiwuc&button_colour=b2f0ec&font_colour=000000&font_family=Bree&outline_colour=000000&coffee_colour=FFDD00"></a>\
 `
 console.log(readme)
