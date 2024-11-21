b37b6b77944ee96cac37d7dd8b1688d960bd8bc5
Jacqui <44564577+jacquiwuc@users.noreply.github.com>
2024-11-21 21:22:36 +1300

    Update index.js

--- index.js
+++ index.js
@@ -18,21 +18,17 @@ const readme = `\
 ### Hi there 👋
 I'm Jacqui, a Data Engineer living and working in Auckland, New Zealand.
 - ☕ I’m currently working in banking as a data engineer.
-- 💻 Current stack: Big Data stack, e.g., Hadoop, Hive, Impala, Scala, Spark, Airflow etc
+- 💻 Current stack: Big Data stack, e.g., Hadoop, Hive, Python, Scala, Spark, Airflow etc
 - 💻 Previous project: build a cloud data analytics infrastructure, using AWS, Snowflake, Terraform, DBT etc
 - 📫 How to reach me: 
      - [LinkedIn](https://www.linkedin.com/in/jacqui-wu/) 
  
----
-### My GitHub Contributions    
-
-![](https://raw.githubusercontent.com/phh95/phh95/main/assets/github-contribution-grid-snake.svg)
 
 ---
 ⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
 
 ⏰ Updated on ${new Date().toUTCString()}
-![Progress Bar CI](https://github.com/jacquiwuc/jacquiwuc/workflows/Progress%20Bar%20CI/badge.svg)
+![Progress Bar CI]
 
 `
 console.log(readme)
